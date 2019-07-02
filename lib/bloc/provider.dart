45aa23eff67192cae966a2f1748d350b192bcff7
rodrigoh00per <rodrigoh00per1@gmail.com>
2019-07-02 13:06:25 -0500

    Cambios en  combineStream

---
+++ lib/bloc/provider.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+import 'login_bloc.dart';
+export 'login_bloc.dart';
+
+class Provider extends InheritedWidget {
+  final loginBloc = LoginBloc();
+
+  Provider({Key key, Widget child}) : super(key: key, child: child);
+
+  @override
+  bool updateShouldNotify(Provider oldWidget) => true;
+
+  static LoginBloc of(BuildContext context) {
+    return (context.inheritFromWidgetOfExactType(Provider) as Provider)
+        .loginBloc;
+  }
+}
+
+//este nos permite unicamente regresar una unica instancia de loginBloc
